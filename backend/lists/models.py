5f89aedf3a09df750098bf927e6e6e8b11c1c5a0
ShahabAthar25 <shahabathar25@gmail.com>
2024-05-20 17:30:36 +0500

    Add lists model

---
+++ backend/lists/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MinLengthValidator
+
+from tweets.models import Tweet
+
+User = get_user_model()
+
+class List(models.Model):
+    users = models.ManyToManyField(User, related_name="users_set")
+    tweets = models.ManyToManyField(Tweet, related_name="tweets_set")
+    title = models.CharField(max_length=255, validators=[MinLengthValidator(10)])
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.title}"
