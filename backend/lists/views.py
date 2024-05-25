8d061af792538de60285a4a1b287500dba278f57
ShahabAthar25 <shahabathar25@gmail.com>
2024-05-25 19:44:07 +0500

    Add routes for listing followed and following users of a list

--- backend/lists/views.py
+++ backend/lists/views.py
@@ -1,8 +1,10 @@
 from rest_framework import generics
+from django.shortcuts import get_object_or_404
 
 from .models import List
 from .serializers import ListSerializer
 from tweets.permissions import IsOwnerOrReadonly
+from users.serializers import UserSerializer
 
 class ListCreateListsView(generics.ListCreateAPIView):
     queryset = List.objects.all()
@@ -13,3 +15,17 @@ class RetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ListSerializer
     permission_classes = (IsOwnerOrReadonly,)
     lookup_field = "pk"
+
+class FollowingListUsersView(generics.ListAPIView):
+    serializer_class = UserSerializer
+    
+    def get_queryset(self):
+        _list = get_object_or_404(List, id=self.kwargs["pk"])
+        return _list.following_users.all()
+
+class FollowedListUsersView(generics.ListAPIView):
+    serializer_class = UserSerializer
+    
+    def get_queryset(self):
+        _list = get_object_or_404(List, id=self.kwargs["pk"])
+        return _list.followed_users.all()
